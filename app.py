f31e6ac2b027a86b39b5d2417046d23e29f89373
Shariq29 <shariqsayeed33@gmail.com>
2025-11-23 23:04:58 +0530

    Banglore House Price Prediction

--- app.py
+++ app.py
@@ -1,31 +1,33 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 import util
 
 app = Flask(__name__)
-CORS(app)
 
-@app.route('/')
-def home():
-    return "Home Price Prediction API is running!"
 
 @app.route('/get_location_names', methods=['GET'])
 def get_location_names():
-    return jsonify({'locations': util.get_location_names()})
+    response = jsonify({
+        'locations': util.get_location_names()
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
 
-@app.route('/predict_home_price', methods=['POST'])
+    return response
+
+@app.route('/predict_home_price', methods=['GET', 'POST'])
 def predict_home_price():
-    # Accept JSON input
-    data = request.get_json()
-    total_sqft = float(data['total_sqft'])
-    location = data['location']
-    bhk = int(data['bhk'])
-    bath = int(data['bath'])
+    total_sqft = float(request.form['total_sqft'])
+    location = request.form['location']
+    bhk = int(request.form['bhk'])
+    bath = int(request.form['bath'])
 
-    estimated_price = util.get_estimated_price(location, total_sqft, bhk, bath)
-    return jsonify({'estimated_price': estimated_price})
+    response = jsonify({
+        'estimated_price': util.get_estimated_price(location,total_sqft,bhk,bath)
+    })
+    response.headers.add('Access-Control-Allow-Origin', '*')
+
+    return response
 
 if __name__ == "__main__":
     print("Starting Python Flask Server For Home Price Prediction...")
     util.load_saved_artifacts()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run()
