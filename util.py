4267d493f746058b92169114ac7a8204082f89ad
Shariq29 <shariqsayeed33@gmail.com>
2025-11-26 18:15:11 +0530

    Banglore House Price Prediction

--- util.py
+++ util.py
@@ -2,12 +2,50 @@
 import pickle
 import json
 import numpy as np
+import os
 
 __locations = None
 __data_columns = None
 __model = None
 
-def get_estimated_price(location,sqft,bhk,bath):
+
+def load_saved_artifacts():
+    print("loading saved artifacts...start")
+
+    global __data_columns
+    global __locations
+    global __model
+
+    # Get project root folder (where util.py is)
+    root_path = os.path.dirname(os.path.abspath(__file__))
+
+    # Correct path to /server/artifacts/
+    artifacts_path = os.path.join(root_path, "server", "artifacts")
+
+    columns_path = os.path.join(artifacts_path, "columns.json")
+    model_path = os.path.join(artifacts_path, "banglore_home_prices_model.pickle")
+
+    # Load columns.json
+    with open(columns_path, "r") as f:
+        __data_columns = json.load(f)["data_columns"]
+        __locations = __data_columns[3:]
+
+    # Load model
+    with open(model_path, "rb") as f:
+        __model = pickle.load(f)
+
+    print("loading saved artifacts...done")
+
+
+def get_location_names():
+    return __locations
+
+
+def get_data_columns():
+    return __data_columns
+
+
+def get_estimated_price(location, sqft, bhk, bath):
     try:
         loc_index = __data_columns.index(location.lower())
     except:
@@ -17,33 +55,17 @@ def get_estimated_price(location,sqft,bhk,bath):
     x[0] = sqft
     x[1] = bath
     x[2] = bhk
-    if loc_index>=0:
+
+    if loc_index >= 0:
         x[loc_index] = 1
 
-    return round(__model.predict([x])[0],2)
+    return round(__model.predict([x])[0], 2)
 
 
-def load_saved_artifacts():
-    print("loading saved artifacts...start")
-    global __data_columns
-    global __locations
-    global __model
+# Auto-load artifacts
+load_saved_artifacts()
 
-    import os
-    base_path = os.path.dirname(__file__)
-    artifacts_path = os.path.join(base_path, "artifacts")
 
-    columns_path = os.path.join(artifacts_path, "columns.json")
-    model_path = os.path.join(artifacts_path, "banglore_home_prices_model.pickle")
-
-    with open(columns_path, "r") as f:
-        __data_columns = json.load(f)['data_columns']
-        __locations = __data_columns[3:]
-
-    with open(model_path, 'rb') as f:
-        __model = pickle.load(f)
-
-    print("loading saved artifacts...done")
 
 
 if __name__ == '__main__':
